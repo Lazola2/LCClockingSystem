a1c1c2d73a1f522485c4e57d95dac2b69d510550
Lazola2 <lazolamakubalo271@gmail.com>
2023-06-13 11:12:08 +0200

    add interface called Clocking Repository

---
+++ src/main/java/com/lcclockingsystem/sbcrud/clocking/ClockingRepository.java
@@ -0,0 +1,5 @@
+package com.lcclockingsystem.sbcrud.clocking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ClockingRepository  extends JpaRepository<Clocking, Integer> {
+}
