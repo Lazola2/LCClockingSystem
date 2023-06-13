454e8cc9e24ef7454a149415c0844f85f4e12356
Lazola2 <lazolamakubalo271@gmail.com>
2023-06-13 08:53:39 +0200

    remove unnecessary comments

--- src/main/java/com/lcclockingsystem/sbcrud/users/UserController.java
+++ src/main/java/com/lcclockingsystem/sbcrud/users/UserController.java
@@ -2,7 +2,6 @@ package com.lcclockingsystem.sbcrud.users;
 
 import lombok.AllArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
